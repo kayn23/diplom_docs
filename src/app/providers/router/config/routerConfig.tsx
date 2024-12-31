7ef587a05aaa0921d1aa166504904e092ec82811
Anatoly Chernyshev <kayn23@yandex.ru>
2024-12-31 04:00:20 +0300

    feat(pages): add notFoundPage

--- src/app/providers/router/config/routerConfig.tsx
+++ src/app/providers/router/config/routerConfig.tsx
@@ -1,6 +1,7 @@
 import { AboutPage } from 'pages/AboutPage';
 import { MainPage } from 'pages/MainPage';
-import { AppRoutes, getRouteAbout, getRouteMain } from 'shared/const/router';
+import { NotFoundPage } from 'pages/NotFoundPage';
+import { AppRoutes, getRouteAbout, getRouteMain, getRouteNotFound } from 'shared/const/router';
 import { AppRoutesProps } from 'shared/types/router';
 
 export const routerConfig: Record<AppRoutes, AppRoutesProps> = {
@@ -11,5 +12,9 @@ export const routerConfig: Record<AppRoutes, AppRoutesProps> = {
   [AppRoutes.ABOUT]: {
     path: getRouteAbout(),
     element: <AboutPage />,
+  },
+  [AppRoutes.NOT_FOUND]: {
+    path: getRouteNotFound(),
+    element: <NotFoundPage />,
   },
 };
