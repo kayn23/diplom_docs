42ff748e1be5bd7946c5dab4bf72f0f7cd6d45d8
Anatoly Chernyshev <kayn23@yandex.ru>
2025-03-15 00:14:36 +0300

    feat: added shipping finisher service

---
+++ app/services/shipping_finisher_service.rb
@@ -0,0 +1,23 @@
+class ShippingFinisherService
+  class ShippingCantFinishError < StandardError
+    def initialize
+      super 'shipping cannot finish'
+    end
+  end
+
+  attr_accessor :shipping
+
+  def initialize(shipping_id)
+    @shipping = Shipping.eager_load(:cargo_in_shippings).find(shipping_id)
+  end
+
+  def call
+    raise ShippingCantFinishError unless shipping.may_finish?
+
+    return unless shipping.cargo_in_shippings.all? do |item|
+      item.delivered?
+    end
+
+    shipping.finish!
+  end
+end
